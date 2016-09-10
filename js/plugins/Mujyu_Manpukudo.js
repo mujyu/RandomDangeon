9cb02f433d06d9835fa963170c75a9a380266ef0
mujyu <mujyuapp@gmail.com>
2016-09-11 01:09:34 +0900

    村制作中

--- js/plugins/Mujyu_Manpukudo.js
+++
@@ -1,7 +0,0 @@
-$gameVariables.setValue(6, $gameParty.members()[0].fig);
-if ($gameParty.members()[0].fig >= 100) {
-    $gameVariables.setValue(6, 0);
-    $gameParty.members()[0].setLifeparams(2, 0);
-    this.setupChild($dataCommonEvents[6].list, 0);
-    this.setupChild($dataCommonEvents[7].list, 0);
-}
